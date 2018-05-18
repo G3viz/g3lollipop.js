460003d150fcfb083bc2b6163565acf851b048aa
phoeguo <phoeguo@gmail.com>
2018-05-18 16:47:40 -0700

    g3-lollipop v0.0.1

---
+++ index.js
@@ -0,0 +1,7 @@
+export { getTextWidth, getUniqueID, appendValueToDomain } from "./src/utils/utils";
+export { palettes, defaultPalette, getPalette, listPalettes, scaleOrdinal } from "./src/utils/color";
+export { default as output } from "./src/output/output";
+export { default as legend } from "./src/legend/legend";
+export { default as tooltip } from "./src/utils/tooltip";
+
+export { default as Lollipop } from "./src/lollipop/lollipop";
---
+++ rollup.config.js
@@ -0,0 +1,20 @@
+import resolve from 'rollup-plugin-node-resolve';
+
+export default {
+    external: [
+        'd3'
+    ],
+    input: 'index.js',
+    output: {
+        extend: true,
+        file: "dist/g3-lollipop.js",
+        format: "umd",
+        name: "g3",
+        indent: false,
+        globals: {
+            "d3": "d3"
+        },
+    },
+    plugins: [resolve()],
+};
+
