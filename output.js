a5e1ceb3a2679565e9acd4f49e58f313b520ab08
phoeguo <phoeguo@gmail.com>
2019-01-31 14:31:14 -0800

    minor changes

--- output.js
+++ output.js
@@ -1,3 +1,3 @@
 export { getTextWidth, getUniqueID } from "./src/utils/utils";
-export { palettes, defaultPalette, getPalette, listPalettes, scaleOrdinal } from "./src/utils/color";
+export { palettes, defaultPalette, getPalette, listPalettes } from "./src/utils/color-utils";
 export { default as output } from "./src/output/output";
